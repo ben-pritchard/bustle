415b5778f58ef517046b9eed031e11e1fd178783
Ben Pritchard and Monty Rohde <ben.s.pritchard@gmail.com>
2015-03-02 14:00:45 -0800

    Add links that require router for pictures to stories.

--- router.js
+++ router.js
@@ -1,4 +1,7 @@
 Bustle.Router.map(function() {
   this.resource('posts', {path: "/"});
   this.resource('headline1');
+  this.resource('headline2');
+  this.resource('headline3');
+  this.resource('headline4');
 });
