75451d769f17da60d21e6136749cec44ae4b3169
Ben Pritchard and Monty Rohde <nathanarohde@gmail.com>
2015-03-02 17:02:18 -0800

    Added controller to about page which allows for an image to be shown or hidden.

---
+++ controllers/about.js
@@ -0,0 +1,11 @@
+Bustle.AboutController = Ember.Controller.extend({
+  isPictureShowing: false,
+  actions: {
+    showPicture: function(){
+      this.set('isPictureShowing',true);
+    },
+    hidePicture: function(){
+      this.set('isPictureShowing',false);
+    }
+  }
+});
